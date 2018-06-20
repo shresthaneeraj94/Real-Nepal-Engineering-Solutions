91066c6cd3fde154ab7d2adf22a82ca2c4215334
b7Rexx <bij.maharjan@gmail.com>
2018-06-20 10:34:37 +0545

    admin page

---
+++ @dmin/Includes/footer.php
@@ -0,0 +1,15 @@
+<!-- footer content -->
+<footer>
+    <div class="pull-right">
+        Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+    </div>
+    <div class="clearfix"></div>
+</footer>
+<!-- /footer content -->
+</div>
+</div>
+<script src="gentelella/jquery.min.js"></script>
+<script src="gentelella/bootstrap.min.js"></script>
+<script src="gentelella/custom.min.js"></script>
+</body>
+</html>
---
+++ @dmin/Includes/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RNES - Admin Panel</title>
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+    <link rel="stylesheet" href="gentelella/bootstrap.min.css">
+    <link rel="stylesheet" href="gentelella/nprogress.css">
+    <link rel="stylesheet" href="gentelella/custom.min.css">
+</head>
+<body class="nav-md">
+<div class="container body">
+    <div class="main_container">
