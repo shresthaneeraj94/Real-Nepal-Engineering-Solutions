a4e649c38e8143fb3d74887b77796eb07c1165f8
b7Rexx <bij.maharjan@gmail.com>
2018-06-29 22:10:28 +0545

    mail

--- @dmin/Mail.php
+++ @dmin/Mail.php
@@ -2,10 +2,28 @@
 include_once "Includes/authCheck.php";
 include_once 'action/DBConnect.php';
 //mail list
-$sql = "SELECT * FROM mail";
+$sql = "SELECT * FROM mail ORDER BY id DESC";
 $query = $conn->prepare($sql);
 $query->execute();
-$list = $query->fetchAll();
+$output = $query->fetchAll();
+
+$total = count($output);
+$limit = 25;
+$number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+$pages = intval($total / $limit);
+if ($total % $limit) {
+    $pages++;
+}
+$list = [];
+$page_number = isset($_GET['pages']) ? $_GET['pages'] : 1;
+
+foreach ($output as $key => $data) {
+    if ($key < ($page_number - 1) * $limit) {
+    } elseif ($key >= $page_number * $limit) {
+    } else {
+        $list[$key] = $data;
+    }
+}
 
 include_once "Includes/header.php";
 include_once "Includes/nav.php";
@@ -55,17 +73,19 @@ include_once "Includes/nav.php";
                                 <table class="table table-striped">
                                     <tr>
                                         <th>S/n</th>
-                                        <th width="45%">subject</th>
-                                        <th width="25%">sender</th>
+                                        <th width="40%">subject</th>
+                                        <th width="20%">sender</th>
                                         <th width="25%">sender(email)</th>
+                                        <th width="10%">time</th>
                                     </tr>
                                     <?php foreach ($list as $key => $data) { ?>
                                         <tr v-on:click="mailDetail(<?= $data['id'] ?>)"
-                                            class="mail-table mail-<?= $data['status']?>">
+                                            class="mail-table mail-<?= $data['status'] ?>">
                                             <td><?= ++$key ?></td>
                                             <td><?= $data['subject'] ?></td>
                                             <td><?= $data['name'] ?></td>
                                             <td><?= $data['email'] ?></td>
+                                            <td><?= $data['created_at'] ?></td>
                                         </tr>
                                     <?php } ?>
                                 </table>
@@ -75,10 +95,11 @@ include_once "Includes/nav.php";
                                         <img src="/@dmin/docs/logo.png" alt="image">
                                         &nbsp;&nbsp;&nbsp;Real Nepal Engineering Solutions
                                     </h4>
-                                    <div style="border: 2px solid lightgray;padding: 20px">
+                                    <div style="border: 2px solid lightgray;background:ghostwhite;padding: 20px">
                                         <b>
                                             Sender Info :
                                             <br><i class="fa fa-user"></i> Name : {{mailData.name}}
+                                            <b class="alignright">Date : {{mailData.created_at}}</b>
                                             <br><i class="fa fa-envelope"></i> Email : {{mailData.email}}
                                             <br><i class="fa fa-phone"></i> Phone : {{mailData.phone}}
                                         </b>
@@ -97,6 +118,15 @@ include_once "Includes/nav.php";
 
                             </div>
                             <!--content here-->
+                            <div class="alignright">
+                                Pages |
+                                <?php
+                                foreach ($number as $page) {
+                                    if ($page <= $pages)
+                                        echo "<div style='display: inline'><a href='?pages=" . $page . "' class=\"btn btn-default btn-sm\"><b>" . $page . "</b></a></div>";
+                                }
+                                ?>
+                            </div>
                         </div>
                     </div>
                 </div>
